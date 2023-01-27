8649e8bf0739bf1afedee89305fc23a226c5b637
Shudhanshu Pandey <shudhanshu.pandey@botmantra.com>
2023-01-27 14:23:08 +0530

    check App.js (use rcc to generate snippet)

---
+++ code_with_harry/news_app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import React, { Component } from 'react'
+
+export default class App extends Component {
+  name="shudhanshu"
+  render() {
+    return (
+      <div>
+        welcome first class based component {this.name}
+        
+      </div>
+    )
+  }
+}

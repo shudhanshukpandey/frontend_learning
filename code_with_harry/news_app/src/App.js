c7fa39861f8ec4c9ca976b94fe5bbab044735ac8
Shudhanshu Pandey <shudhanshu.pandey@botmantra.com>
2023-01-30 17:15:28 +0530

    react router dom added along with configuring navlinks

--- code_with_harry/news_app/src/App.js
+++ code_with_harry/news_app/src/App.js
@@ -3,6 +3,10 @@ import './App.css';
 import React, { Component } from 'react'    // use rcc to generate snippet for class based component
 import NavBar from './components/NavBar';
 import News from './components/News';
+import {
+  Routes,
+  Route
+} from 'react-router-dom'
 
 export default class App extends Component {
   name="shudhanshu"
@@ -11,10 +15,21 @@ export default class App extends Component {
       <div>
 
         <NavBar/>
-
-        <News country="in" catogery="sports"/>
-        
+        {/* <News country="in" catogery="general"/> */}
+        <Routes>
+          <Route exact path="/" element=             {<News key="general" country="in" catogery="general"/>}/>
+          <Route exact path="/about" element=        {<News key="about" country="in" catogery="about"/>}/>
+          <Route exact path="/business" element=     {<News key="business" country="in" catogery="business"/>}/>
+          <Route exact path="/entertainment" element={<News key="entertainment" country="in" catogery="entertainment"/>}/>
+          <Route exact path="/science" element=      {<News key="science" country="in" catogery="science"/>}/>
+          <Route exact path="/general" element=      {<News key="general" country="in" catogery="general"/>}/>
+          <Route exact path="/health" element=       {<News key="health" country="in" catogery="health"/>}/>
+          <Route exact path="/sports" element=       {<News key="sports" country="in" catogery="sports"/>}/>
+          <Route exact path="/tech" element=         {<News key="tech" country="in" catogery="technology"/>}/>
+        </Routes>         
+                 
       </div>
-    )
+    )         
   }
 }
+// keys are needed if same component is called again and again if not then react will not be able to find with props to pass at the moment
--- code_with_harry/news_app/src/components/NavBar.js
+++ code_with_harry/news_app/src/components/NavBar.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react'
-import PropTypes from 'prop-types'                //rcep is used
+import PropTypes from 'prop-types'     //rcep is used
 
+
+import { NavLink } from 'react-router-dom'
 export class NavBar extends Component {           // input and hr tag needs to be closed with '/'
   static propTypes = {
-                                                    // href="#" will cause issue change it to '/' or '/link_name'
+                                                    // to="#" will cause issue change it to '/' or '/link_name'
   }
 
   render() {
@@ -12,26 +14,26 @@ export class NavBar extends Component {           // input and hr tag needs to b
 
 <nav className="navbar navbar-expand-lg bg-body-tertiary">
   <div className="container-fluid">
-    <a className="navbar-brand" href="/">News-App</a>
+    <NavLink className="navbar-brand" to="/">News-App</NavLink>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span className="navbar-toggler-icon"></span>
     </button>
     <div className="collapse navbar-collapse" id="navbarSupportedContent">
       <ul className="navbar-nav me-auto mb-2 mb-lg-0">
         <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Home</a>
+          <NavLink className="nav-link active" aria-current="page" to="/">Home</NavLink>
         </li>
-        <li className="nav-item"><a className="nav-link" href="/">About</a></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/about">About</NavLink></li>
 
         {/*adding multi catogeroies for news */}
 
-        <li className="nav-item"><a className="nav-link" href="/">business</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">entertaainment</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">general</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">health</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">science</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">sports</a></li>
-        <li className="nav-item"><a className="nav-link" href="/">techonology</a></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/business">business</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/entertainment">entertaainment</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/general">general</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/health">health</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/science">science</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/sports">sports</NavLink></li>
+        <li className="nav-item"><NavLink className="nav-link" to="/tech">techonology</NavLink></li>
       {/* use alt+shift+ctrl + arrow */}
 
 

8656928324a9692af459037c317500f61911bbd6
Shudhanshu Pandey <shudhanshu.pandey@botmantra.com>
2023-02-10 14:51:41 +0530

    validation added and priimary key email, catched errors

--- code_with_harry/MERN_Project/inotebook-backend/routes/auth.js
+++ code_with_harry/MERN_Project/inotebook-backend/routes/auth.js
@@ -6,6 +6,9 @@ const User=require("../models/User");
 const router=express.Router();     //works similar to blueprint
 
 
+const { body, validationResult } = require('express-validator');      //for validation (marshmellow)
+
+
 
 
 router.get("/get_req" ,(req,res)=>{            //req is for request body (payload), res is for response(jsonify)
@@ -24,7 +27,7 @@ router.get("/" ,(req,res)=>{            //req is for request body (payload), res
 
 })
 
-router.get("/test" ,(req,res)=>{
+router.get("/test" ,(req,res)=>{            // test api
     obj={
         status:true,
         message:"auth test api working"    }
@@ -33,14 +36,46 @@ router.get("/test" ,(req,res)=>{
 })
 
 //create a new user
-router.post("/sign_up",(req,res)=>{
-    user_data=req.body;
+router.post("/sign_up",(req,res)=>{           //signup api
+    let user_data=req.body;
     const user=User(user_data);
     user.save()
     console.log("user created")
     res.json({
         "status":true,
         "message":"user created"
+    })
+})
+
+router.post("/sign_up_validate",                    //validation api
+[
+    body('name',"enter a valid name").isLength({min:5}),      // first parameter is key second is message in case invalid
+    body('password').isLength({min:5}),
+    body('email').isEmail(),
+]
+
+,(req,res)=>{
+    const errors=validationResult(req);
+    if (!errors.isEmpty())
+    {
+        return res.status(400).json({
+            errors:errors.array()
+        })
+    }
+
+    User.create(
+        {
+            name:req.body.name,
+            password:req.body.password,
+            email:req.body.email
+        }
+    ).then(user=>res.json(user)).catch(err=>{
+        
+        console.log(err)
+        res.json({
+            "error":"enter proper key",
+            "message":err.message
+        })
     })
 })
 
